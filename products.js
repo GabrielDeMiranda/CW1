3ff842ee785712e827266fbff8071773322b4777
GabrielDeMiranda <73244609+GabrielDeMiranda@users.noreply.github.com>
2020-11-19 10:29:01 +0000

    Add files via upload

---
+++ products.js
@@ -0,0 +1,100 @@
+let products = [
+    {
+        "id": 1001,
+        "title": "Maths",
+        "description": "Class of Maths",
+        "price": 10,
+        "image": "",
+        "availableInventory": 5,
+        "location": "London"
+
+},
+{
+    "id": 1002,
+    "title": "Science",
+    "description": "Class of Science",
+    "price": 15,
+    "image": "",
+    "availableInventory": 5,
+    "location": "London"
+
+},
+{
+    "id": 1003,
+    "title": "Physics",
+    "description": "Class of Physics",
+    "price": 20,
+    "image": "",
+    "availableInventory": 5,
+    "location": "London"
+
+},
+{
+    "id": 1004,
+    "title": "Geometry",
+    "description": "Class of Geomtry",
+    "price": 22,
+    "image": "",
+    "availableInventory": 5,
+    "location": "London"
+
+},
+{
+    "id": 1005,
+    "title": "WebApp",
+    "description": "Class of WebApp",
+    "price": 99,
+    "image": "",
+    "availableInventory": 5,
+    "location": "London"
+},
+{
+    "id": 1006,
+    "title": "Maths",
+    "description": "Class of Maths",
+    "price": 10,
+    "image": "",
+    "availableInventory": 5,
+    "location": "Cambridge"
+
+},
+{
+"id": 1007,
+"title": "Science",
+"description": "Class of Science",
+"price": 15,
+"image": "",
+"availableInventory": 5,
+"location": "Cambridge"
+
+},
+{
+"id": 1008,
+"title": "Physics",
+"description": "Class of Physics",
+"price": 20,
+"image": "",
+"availableInventory": 5,
+"location": "Cambridge"
+
+},
+{
+"id": 1009,
+"title": "Geometry",
+"description": "Class of Geomtry",
+"price": 22,
+"image": "",
+"availableInventory": 5,
+"location": "Cambridge"
+
+},
+{
+"id": 1010,
+"title": "WebApp",
+"description": "Class of WebApp",
+"price": 99,
+"image": "",
+"availableInventory": 5,
+"location": "Cambridge"
+},
+]
